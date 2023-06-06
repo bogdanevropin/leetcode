95a17ae0d70dc4d3751c94412a0d73ec2adaeb66
MindAbuzer <bogdanevropin@gmail.com>
2023-06-06 12:30:34 +0300

    06/06

--- Array_Hashing/leetcode_128_Longest_Consecutive_Sequence.py
+++ Array_Hashing/leetcode_128_Longest_Consecutive_Sequence.py
@@ -4,3 +4,17 @@ return the length of the longest consecutive elements sequence.
 You must write an algorithm that runs in O(n) time.
 """
 
+
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+        for n in nums:
+            # check if that a start of sequence
+            prev = n - 1
+            if prev not in numSet:  # start of sequence
+                length = 0
+                while length + n in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
--- Array_Hashing/leetcode_347_Top_K_Frequent_Elements.py
+++ Array_Hashing/leetcode_347_Top_K_Frequent_Elements.py
@@ -6,16 +6,27 @@ You may return the answer in any order.
 
 class Solution:
 	@staticmethod
-    def topKFrequent1(nums: list[int], k: int) -> list[int]:
-        all_nums_f = {}
-        for num in nums:
-            if num not in all_nums_f:
-                all_nums_f[num] = 1
-            else:
-                all_nums_f[num] += 1
-        return [kv[0] for kv in sorted(all_nums_f.items(),
-                                       key=lambda item: item[1])[-k:]]
+	def topKFrequent1(nums: list[int], k: int) -> list[int]:
+		all_nums_f = {}
+		for num in nums:
+			if num not in all_nums_f:
+				all_nums_f[num] = 1
+			else:
+				all_nums_f[num] += 1
+				return [kv[0] for kv in sorted(all_nums_f.items(),
+				                               key=lambda item: item[1])[-k:]]
         
-		@staticmethod
-		def topKFrequent2(nums: list[int], k: int) -> list[int]:
-		
+	@staticmethod
+	def topKFrequent2(nums: list[int], k: int) -> list[int]:
+		count = {}
+		freq = [[] for _ in range(len(nums) + 1)]
+		for n in nums:
+			count[n] = 1 + count.get(n, 0)
+		for n, c in count.items():
+			freq[c].append(n)
+		res = []
+		for i in range(len(freq) - 1, 0, -1):
+			for n in freq[i]:
+				res.append(n)
+				if len(res) == k:
+					return res
